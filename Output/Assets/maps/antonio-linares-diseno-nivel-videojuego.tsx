bd4ecbc5840daf3350c3f5a7343637b1bc8a9c5d
Gerard Martinez <gerardmartinez2002@gmail.com>
2021-10-30 00:52:41 +0200

    New Git

---
+++ Output/Assets/maps/antonio-linares-diseno-nivel-videojuego.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="antonio-linares-diseno-nivel-videojuego" tilewidth="32" tileheight="32" margin="1" tilecount="11026" columns="149">
+ <image source="antonio-linares-diseno-nivel-videojuego.jpg" width="4800" height="2400"/>
+</tileset>
